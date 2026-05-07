adfdf5412d71f48cafc5df5e86d6d85acb926d1d
emg136-gh <emg136@gmail.com>
2026-05-07 17:15:56 +0000

    modified

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 01_02
+// Exercise 01_02 successful build
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Good day to you" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
